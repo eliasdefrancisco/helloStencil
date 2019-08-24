ef134db413bd533d58d2e3e0f3dac069aa96d88e
Elias de Francisco <eliasdefrancisco@gmail.com>
2019-08-24 19:08:19 +0200

    Some styling

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,14 +1,16 @@
 import { Component, h } from '@stencil/core'
 
 @Component({
-    tag: 'my-side-drawer'
+    tag: 'my-side-drawer',
+    styleUrl: './side-drawer.css',
+    shadow: true
 })
 export class SideDrawer {
     render() {
         return (
-            <div>
+            <aside>
                 <h1>The Side Drawer</h1>
-            </div>
+            </aside>
         )
     }
 }
